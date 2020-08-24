bb3d619dbb33bbddd5d83419aabd777085490e15
Tobias Laundal <tobias.laundal@ardoq.com>
2020-08-24 20:20:30 +0200

    Test that add correctly adds floats

--- test_calculator.py
+++ test_calculator.py
@@ -2,3 +2,11 @@ import calculator
 
 def test_add():
     assert calculator.add(1, 2) == 3
+
+def test_add_floats():
+    expected = 0.3
+    tolerance = 1e-14
+
+    actual = calculator.add(0.1, 0.2)
+
+    assert abs(actual - expected) < tolerance
