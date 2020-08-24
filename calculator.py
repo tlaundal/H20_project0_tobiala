b0fedb0c32cf2c0326306fd31b3b48ecaaae7d7d
Tobias Laundal <tobias.laundal@ardoq.com>
2020-08-24 20:29:57 +0200

    Implement more calculator functions
    Adds functions for calculating the factorial, sin, dividing, multiplying
    and subtracting, with corresponding tets.

--- calculator.py
+++ calculator.py
@@ -1,2 +1,24 @@
+import math
+
 def add(x, y):
     return x + y
+
+def factorial(n):
+    if n < 2:
+        return 1
+    return n * factorial(n-1)
+
+def sin(x, N):
+    r = 0
+    for n in range(N+1):
+        r += math.pow(-1, n)*math.pow(x, 2*n+1)/factorial(2*n+1)
+    return r
+
+def divide(x, y):
+    return x / y
+
+def multiply(x, y):
+    return x * y
+
+def subtract(x, y):
+    return x - y
--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,4 @@
+import math
 import calculator
 
 def test_add():
@@ -13,3 +14,23 @@ def test_add_floats():
 
 def test_add_strings():
     assert calculator.add("Hello ", "World") == "Hello World"
+
+def test_factorial():
+    assert calculator.factorial(7) == math.factorial(7)
+
+def test_sin():
+    expected = math.sin(2)
+    tolerance = 1e-4
+
+    actual = calculator.sin(2, 5)
+
+    assert abs(actual - expected) < tolerance
+
+def test_divide():
+    assert calculator.divide(10, 2) == 5
+
+def test_multiply():
+    assert calculator.multiply(5, 2) == 10
+
+def test_subtract():
+    assert calculator.subtract(2, 4) == -2
